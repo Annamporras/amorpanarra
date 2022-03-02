c6085f72368e0d734017c7fa3ee4d80fe7c81413
Anna Ma Porras <amporras85@gmail.com>
2022-03-02 10:53:04 +0100

    logo docs services & components

---
+++ client/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const Navigation = () => {
+
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="warning" variant="light">
+            <Container>
+                <Navbar.Brand href="/"> <img
+                    src="../../images/Logo.png"
+                    width="100"
+                    height="50"
+                    className="d-inline-block align-top"
+                    alt="Amor Panarra logo"
+                /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/productos/crear">Crear Producto</Nav.Link>
+                        <Nav.Link href="/perfiles">Usuarios</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="/perfil">Mi cuenta</Nav.Link>
+                        <Nav.Link eventKey={2} href="/carrito">
+                            Logo carrito
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation
